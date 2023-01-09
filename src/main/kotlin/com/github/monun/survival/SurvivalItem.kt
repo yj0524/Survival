34b4316a948153553581b6eff1f097e5b8f27fa7
yj0524 <jeonyunjae_kr@naver.com>
2023-01-09 14:46:11 +0900

    1.0.8

--- src/main/kotlin/com/github/monun/survival/SurvivalItem.kt
+++ src/main/kotlin/com/github/monun/survival/SurvivalItem.kt
@@ -10,19 +10,19 @@ import org.bukkit.inventory.ItemStack
 object SurvivalItem {
     val greenJuice = ItemStack(Material.SUSPICIOUS_STEW).apply {
         itemMeta = itemMeta.apply {
-            displayName(text("§r§2§l녹즙")) // .color(TextColor.color(0x008000)).decorate(TextDecoration.BOLD)
+            displayName(text("녹즙").color(TextColor.color(0x008000)).decorate(TextDecoration.BOLD))
         }
     }
 
     val vaccine = ItemStack(Material.FIREWORK_ROCKET).apply {
         itemMeta = itemMeta.apply {
-            displayName(text("§r§b§l백신")) // .color(TextColor.color(0x4FAEC9)).decorate(TextDecoration.BOLD)
+            displayName(text("백신").color(TextColor.color(0x4FAEC9)).decorate(TextDecoration.BOLD))
         }
     }
 
     val hyperVaccine = ItemStack(Material.TOTEM_OF_UNDYING).apply {
         itemMeta = itemMeta.apply {
-            displayName(text("§r§c§l하이퍼 백신")) // .color(TextColor.color(0xFF0000)).decorate(TextDecoration.BOLD)
+            displayName(text("하이퍼 백신").color(TextColor.color(0xFF0000)).decorate(TextDecoration.BOLD))
         }
     }
 
